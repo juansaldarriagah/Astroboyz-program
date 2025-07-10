e8400e97fa1e99876f1df8dddb5509469d351587
Crissmann23 <cfmancoh@udistrital.edu.co>
2025-07-10 16:54:07 -0500

    Update SolutionService.java

--- src/service/SolutionService.java
+++ src/service/SolutionService.java
@@ -1,7 +1,40 @@
 package service;
+import java.util.*;
+import model.Solution;
+import utils.PropertiesManager;
 
 public class SolutionService {
-    public void proponerSolucion(String ideaId, String contenido, User autor);
-    public void guardarSoluciones();
-    public void cargarSoluciones();
-}
+	 private final Map<String, Solution> soluciones = new HashMap<>();
+	    private final String FILE_NAME = "solutions.properties";
+	    public SolutionService() {
+	        cargarSoluciones();
+	    }
+
+	    public void proponerSolucion(String ideaId, String content, String autor) {
+	        String id = UUID.randomUUID().toString();
+	        Solution s = new Solution(id, ideaId, content, autor);
+	        soluciones.put(id, s);
+	        guardarSoluciones();
+	    }
+	    public void cargarSoluciones() {
+	        Properties props = PropertiesManager.cargarProperties(FILE_NAME);
+	        for (String key : props.stringPropertyNames()) {
+	            Solution s = Solution.fromProperties(key, props.getProperty(key));
+	            if (s != null) soluciones.put(key, s);
+	        }
+	    }
+	    public void guardarSoluciones() {
+	        Properties props = new Properties();
+	        for (Solution s : soluciones.values()) {
+	            props.setProperty(s.getId(), s.toPropertiesFormat());
+	        }
+	        PropertiesManager.guardarProperties(FILE_NAME, props);
+	    }
+	    public List<Solution> listarSolucionesDeIdea(String ideaId) {
+	        List<Solution> result = new ArrayList<>();
+	        for (Solution s : soluciones.values()) {
+	            if (s.getIdeaId().equals(ideaId)) result.add(s);
+	        }
+	        return result;
+	    }
+	}
