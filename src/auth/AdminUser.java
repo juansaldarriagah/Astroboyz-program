7726175ce5cc837b9890667c5ac646ad3d5653cc
juansaldarriagah <jssaldarriagah@udistrital.edu.co>
2025-07-10 18:41:21 -0500

    Update AdminUser.java

--- src/auth/AdminUser.java
+++ src/auth/AdminUser.java
@@ -1,6 +1,38 @@
 package auth;
 
-public class AdminUser extends User {
-	//extra: eliminar ideas/soluciones, gestionar rankings :v
+import model.idea 
+import auth.user
+import service.IdeaService
+import Solution.Service
 
+public class AdminUser extends User {
+private transient UserService userSevice;
+private transient IdeaService ideaService;
+private transient SolutionService solutionService
+
+public AdminUser (String username , String password){
+	super (username , password);
 }
+public void setServicios (UserService userService , IdeaService ideaService , SolutionService solutionService){
+	this.userService = userService;
+	this.ideaService = ideaService;
+	this.solutionService = solutionService;	
+}
+public boolean eliminarUsuario(String username){
+	User user = userService.buscarPorUsername(username);
+	if ( user!= null && !(user instanceof AdminUser)){
+	userService.getUsuarios().remove(user);
+		return true
+}
+	return false
+}
+
+public boolean eliminarIdea (String idIdea){
+	boolean eliminada = ideaService.eliminarIdea(idIdea);
+	if (eliminada){
+		solutionService.eliminarSolucionesDeIdea(idIdea);
+	}
+	return eliminada
+}
+}
+	
