a1d557b53993438a26cf90183cb2528df97a7a4c
juansaldarriagah <jssaldarriagah@udistrital.edu.co>
2025-07-10 15:22:52 -0500

    Update Idea.java

--- src/model/Idea.java
+++ src/model/Idea.java
@@ -1,12 +1,73 @@
 package model;
 import java.util.*;
+import java.io.serializable
 
-public class Idea {
+public class Idea implements serializable {
     private String id;
-    private String titulo;
-    private String descripcion;
+    private String title;
+    private String description;
     private String autorUsername;
-    private int votosAFavor;
-    private int votosEnContra;
-    private List<Solution> soluciones;
+    private int votesInFavor;
+    private int votesAgaints;
+    private List<Solution> solutions;
+
+    // Constructors
+public idea (String id , String title , String description , String autorUsername){
+    this.id= id;
+    this.title=title;
+    this.description=description;
+    this.autorUsername=autorUsername;
+    this.votesInFavor=0;
+    this.votesAgaintsj=0;
+    this.solutions= new arraylist<>();
+}
+
+// getters
+public String getId(){
+    return id;
+}
+
+public String getTitle(){
+    return title;
+}
+
+public String getDescription(){
+    return description;
+}
+
+public String getAuthorUsername(){
+    return autorUsername;
+}
+
+public int getVotesInFavor(){
+    return votesInFavor;
+}
+
+public int getVotesAgaints(){
+    return votesAgaints;
+}
+
+public list<Solution> getSolutions(){
+    return solutions;
+}
+// vote system
+public void addVoteInFavor(){
+    votesInFavor++;
+}
+public void addVoteAgainst(){
+    votesAgains++;
+}
+//add solutions
+public void addSolution(Solution solution){
+    solutions.add(solutions);
+}
+@Override
+public String toString(){
+return "idea: "+ title + "\n" +
+        "Autor: "+ autorUsername + "\n" +
+        "Descripcion: "+ description + "\n" +
+        "Votos a favor "+ votesInFavor + " en contra: " + votesAgainst + "\n" +
+         "Soluciones propuestas: " + solutions.size();
+    
+}
 }
