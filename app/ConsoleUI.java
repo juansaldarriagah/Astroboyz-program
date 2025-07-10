91400632baf3c910f35f2bea503b8dab913ce039
Crissmann23 <cfmancoh@udistrital.edu.co>
2025-07-09 19:32:34 -0500

    Add files via upload

---
+++ app/ConsoleUI.java
@@ -0,0 +1,9 @@
+package app;
+
+public class ConsoleUI {
+    public void mostrarMenuPrincipal(User user);
+    public void mostrarIdeas();
+    public void crearIdea();
+    public void votarIdea();
+    public void verRankings();
+}
