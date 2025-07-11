1bfbbcedcb4c6e80e9512a84f244fe8ed19cc29b
juansaldarriagah <jssaldarriagah@udistrital.edu.co>
2025-07-10 20:50:44 -0500

    Update FileLockManager.java

--- src/sync/FileLockManager.java
+++ src/sync/FileLockManager.java
@@ -1,7 +1,27 @@
 package sync;
 
-public class FileLockManager {
-    public boolean bloquearArchivo(String nombreArchivo);
-    public void liberarArchivo(String nombreArchivo);
-    public boolean estaBloqueado(String nombreArchivo);
+import java.io.*;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+
+public class LockManager {
+
+    public static boolean withFileLock(String filePath, FileOperation operation) {
+        try (RandomAccessFile raf = new RandomAccessFile(filePath, "rw");
+             FileChannel channel = raf.getChannel();
+             FileLock lock = channel.lock()) {
+
+            operation.execute();
+            return true;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @FunctionalInterface
+    public interface FileOperation {
+        void execute() throws IOException;
+    }
 }
